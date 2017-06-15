c14ab4c25726e687eaaa041a4fa61ba812fc767f
Pravin Jadhav <pravinjadhav762@gmail.com>
2017-06-15 09:23:20 +0530

    changes

--- api/controllers/movie.js
+++ api/controllers/movie.js
@@ -1,20 +1,20 @@
  'use strict';
-	// Include our "db"
+	
 	var db = require('../../config/db')();
-    // Exports all the functions to perform on the db
+    
 	module.exports = {getAll, save, getOne, update, delMovie};
     
-	//GET /movie operationId
+	
 	function getAll(req, res, next) {
       res.json({ movies: db.find()});
     }
-	//POST /movie operationId
+	
 	function save(req, res, next) {
 		res.json({success: db.save(req.body), description: "Movie added to the list!"});
 	}
-	//GET /movie/{id} operationId
+	
 	function getOne(req, res, next) {
-		var id = req.swagger.params.id.value; //req.swagger contains the path parameters
+		var id = req.swagger.params.id.value; 
 		var movie = db.find(id);
 		if(movie) {
 			res.json(movie);
@@ -22,9 +22,9 @@
 			res.status(204).send();
 		}		
 	}
-	//PUT /movie/{id} operationId
+	
 	function update(req, res, next) {
-		var id = req.swagger.params.id.value; //req.swagger contains the path parameters
+		var id = req.swagger.params.id.value; 
 		var movie = req.body;
 		if(db.update(id, movie)){
 			res.json({success: 1, description: "Movie updated!"});
@@ -33,9 +33,9 @@
 		}
 		
 	}
-	//DELETE /movie/{id} operationId
+
 	function delMovie(req, res, next) {
-		var id = req.swagger.params.id.value; //req.swagger contains the path parameters
+		var id = req.swagger.params.id.value; 
 		if(db.remove(id)){
 			res.json({success: 1, description: "Movie deleted!"});
 		}else{
